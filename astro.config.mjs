34c3943605de5bc51749f440b4f180a76d257349
Bendik Borge Øttl <bendikoettl@gmail.com>
2025-06-03 14:24:25 +0200

    Added Astro files

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // If you don't need any custom markdown plugins,
+  // you can omit the `markdown` section entirely.
+});
