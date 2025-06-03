189db3a76a7b450135ea6a3838eaac4c2b1dbaf0
Bebdik <bendikoettl@gmail.com>
2025-06-03 14:43:57 +0200

    Added all astro files

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-// astro.config.mjs
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  // If you don't need any custom markdown plugins,
-  // you can omit the `markdown` section entirely.
-});
